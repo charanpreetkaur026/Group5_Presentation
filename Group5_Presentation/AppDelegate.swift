118bfb4c94fd1700984f10ec183ce55ed7b92558
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-11-04 21:25:29 -0500

    [app delegate] 1 function

--- Group5_Presentation/AppDelegate.swift
+++ Group5_Presentation/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        print("AppDelegate:- didFinishLaunchingWithOption function calledk")
         // Override point for customization after application launch.
         return true
     }
@@ -23,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -31,7 +34,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+//    func applicationDidBecomeActive(_ application: UIApplication) {
+//        print("AppDelegate:- applicationDidBecomeActive function called")
+//    }
 
+    
 
 }
 
